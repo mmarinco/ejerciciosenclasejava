1b74e0cfa25401688d2d8183eda9ad8f81b110fb
mmarinco <maribella1410@gmail.com>
2025-02-27 20:42:45 -0500

    Create ex27022025.java

---
+++ ex27022025.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+import javax.swing.text.html.HTMLDocument.HTMLReader.CharacterAction;
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+
+System.out.println("Maribel Marin Cortes");
+    System.out.println("Integer" + 10 + 
+                       "\n Double: " + 3.14 +
+                       "\n Boolean: " + true);
+                       
+    
+    int numint = 9;
+    double numdouble = 8.4;
+    boolean numboolean = false;
+    char numchar = 'A';
+    String numstring = "Maribel"; 
+    Byte edad = 20;
+    short numshort = 10;
+    long numlong = 100;
+    int[ ] numeros = {1,2,3,4,5};
+    String[] nombres = {"Maribel", "Juan", "Pedro"};
+    
+    
+
+    
+    System.out.println("Integer: " + numint + 
+                       "\n tDouble: " + numdouble +
+                       " \n tBoolean: " + numdouble +
+                       " \n tCharacter: " + numchar +
+                       " \n tString: " + numstring +
+                       " \n tByte: " + edad +
+                       " \n tShort: " + numshort +
+                      " \n tLong: " + numlong );
+   
+int num1 = 10;
+  int num2 = 3;
+    int suma = num1 + num2;
+    int resta = num1 - num2;
+    int multiplicacion = num1 * num2;
+    double division = num1 / num2;
+    int modulo = num1 % num2;
+    System.out.println("Suma: " + suma +
+                       "\n Resta: " + resta +
+                       "\n Multiplicacion: " + multiplicacion +
+                       "\n Division: " + division +
+                       "\n Modulo: " + modulo);
+    
+    
+  } 
+  } 
